ecb600104c6bf96c5bda67d4a70436ab60889814
prfnv___ <pentalbymf@mail.ru>
2021-02-14 19:31:39 +0300

    reindexing function

---
+++ src/reindexing.py
@@ -0,0 +1,51 @@
+import pandas as pd
+import numpy as np
+
+
+def reindexing_sessionid_itemid(session_item: pd.DataFrame) -> pd.DataFrame:
+    '''
+    Функция для переиндиксации индеков сессий и товаров.
+    
+    Args:
+        session_item: Датафрейм с пользовательскими сессиями.
+    '''
+    session = session_item['sessionid'].unique() # все уникальные айди клиентов из user_actions
+    session_cat = np.arange(0, len(session), dtype='uint32') # массив с элементами от 0 до n - 1
+
+    # таблица, где каждая строка переводит clientid в номер строки
+    user_mapping = pd.DataFrame({'old': session, 'new': session_cat})
+
+    items = session_item['itemid'].unique() # все уникальные айди товаров из user_actions
+    items_cat = np.arange(0, len(items), dtype='uint32') # массив с элементами от 0 до m - 1
+
+    # таблица, где каждая строка переводит itemid в номер столбца
+    item_mapping = pd.DataFrame({'old': items, 'new': items_cat})
+
+    session_item['sessionid'] = session_item['sessionid'].map(user_mapping.set_index('old')['new'])
+    session_item['itemid'] = session_item['itemid'].map(item_mapping.set_index('old')['new'])
+    return (session_item, user_mapping, item_mapping)
+
+
+def reindexing_clientid_itemid(user_actions: pd.DataFrame) -> pd.DataFrame:
+    '''
+    Функция для переиндиксации индеков клиентов и товаров.
+    
+    Args:
+        user_action: Датафрейм с данными за август.
+    '''
+    
+    clients = user_actions['clientid'].unique() # все уникальные айди клиентов из user_actions
+    clients_cat = np.arange(0, len(clients), dtype='uint32') # массив с элементами от 0 до n-1
+
+    # таблица, где каждая строка переводит clientid в номер строки
+    user_mapping = pd.DataFrame({'old': clients, 'new': clients_cat})
+
+    items = user_actions['itemid'].unique() # все уникальные айди товаров из user_actions
+    items_cat = np.arange(0, len(items), dtype='uint32') # массив с элементами от 0 до m-1
+
+    # таблица, где каждая строка переводит itemid в номер столбца
+    item_mapping = pd.DataFrame({'old': items, 'new': items_cat})
+
+    user_actions['clientid'] = user_actions['clientid'].map(user_mapping.set_index('old')['new'])
+    user_actions['itemid'] = user_actions['itemid'].map(item_mapping.set_index('old')['new'])
+    return (user_actions, user_mapping, item_mapping)
